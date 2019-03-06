411e062835a9b8c2483e7c68441a72b94e51606f
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-03-05 18:28:20 -0600

    added schema

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## bamazon
-Bamazon is CLI node.js app using mysql to ineract inventory of a mock database
+Bamazon is CLI node.js app using mysql to ineract with the inventory of a mock store
 
 ## Install in the app
 1. if you have node.js installed then simple type 'npm install' into your CLI. This will install the necessary dependencies.
--- schema.sql
+++ schema.sql
@@ -1,1 +1,34 @@
-SELECT * FROM bamazon.products;
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(30) NOT NULL,
+department_name VARCHAR(30) NOT NULL,
+price INTEGER(10) NOT NUll,
+stock_quantity INTEGER(10) NOT NULL,
+PRIMARY KEY(id)
+);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Miller High LIfe", "Beer", 15, 100); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Maplewood Son of Juice", "Beer", 12, 20); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Jim Bean", "Spirit", 20, 25); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tito's Vodka", "Spirit", 20, 25); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hot Chips", "Food", 2, 100); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Orange Juice", "Drink", 3, 50); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cranberry Juice", "Drink", 3, 500); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("New Holland Dragon's Milk", "Beer", 12, 10); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hop Butcher Tavern Cut", "Beer", 12, 10); 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Lo Rez Tamarind Shortcut", "Beer", 15, 10); 
+
+
